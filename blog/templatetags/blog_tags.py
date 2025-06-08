294a97d02f03c15587cc3936f1d7b4bdec9be589
kirill <rybalkin.kirya@mail.ru>
2025-06-09 00:23:36 +0300

    добавил разметку маркдаун в тело блога и карту сайта

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
+
 register = template.Library()
 
 @register.simple_tag
@@ -16,6 +19,13 @@ def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts':latest_posts}
 
+
 @register.simple_tag
-def get_most_commented_posts(count=5):
-    return Post.publish.annotate(total_comments=0).order_by('-total_comments')[:count]
+def get_most_commented_posts(count=2):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).exclude(total_comments=0).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
