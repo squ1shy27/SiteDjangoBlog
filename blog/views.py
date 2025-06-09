3f6ff1da9470a29b35e529dde2098f50bd4dd8ff
korybalkin <korybalkin@istokmv.ru>
2025-06-09 17:04:28 +0300

    добавил поиск посимвольно

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,15 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404  # новый импорт
 from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse
-from .froms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.conf import settings
 from taggit.models import Tag
 from django.db.models import Count
 from django.views.decorators.http import require_POST
-
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 # Представление для отображения списка постов и инфо
 def post_list(request, tag_slug=None):
@@ -106,4 +107,24 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
 
